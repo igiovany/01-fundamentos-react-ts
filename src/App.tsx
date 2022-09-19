e46bb1dfc890f8a9c0671c5afe35444378f43504
igiovany <105370259+igiovany@users.noreply.github.com>
2022-09-18 21:47:56 -0300

    Estruturando componentes em TypeScript

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,65 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { Header } from './components/Header';
+import { Post } from './components/Post';
+import { Sidebar } from './components/Sidebar';
 
-function App() {
-  const [count, setCount] = useState(0)
+import styles from './App.module.css';
 
+import './global.css';
+
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/igiovany.png',
+      name: 'Giovany Inocêncio',
+      role: 'Web Developer'
+    },
+    content: [
+      { type: 'paragraph', content: 'Opa, boa noite!' },
+      { type: 'paragraph', content: 'Estou iniciando os estudos em ReactJS na plataforma Ignite da RocketSeat, em breve estarei subindo novos projetos no meu GitHub!' },
+      { type: 'link', content: 'https://github.com/igiovany' },  
+    ],
+    publishedAt: new Date('2022-09-09 20:00:00'),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/guuhschmaltz.png',
+      name: 'Gustavo Schmaltz',
+      role: 'Software Developer'
+    },
+    content: [
+      { type: 'paragraph', content: 'Apenas um teste' },
+      { type: 'paragraph', content: 'Testando o conteúdo do post!' },
+      { type: 'link', content: 'https://github.com/igiovany' },  
+    ],
+    publishedAt: new Date('2022-09-10 10:00:00'),
+  },
+];
+
+export function App() {
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div>
+      <Header />
+      
+      <div className={ styles.wrapper }>
+        <Sidebar />
+        <main>
+          {posts.map(post => {
+            return (
+              <Post
+                key={post.id}
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            )
+          })}
+        </main>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
     </div>
+    
+    
   )
 }
-
-export default App
---
+++ src/components/Comment.tsx
@@ -0,0 +1,45 @@
+import { ThumbsUp, Trash } from 'phosphor-react';
+import { useState } from 'react';
+import { Avatar } from './Avatar';
+import styles from './Comment.module.css';
+
+export function Comment({ content, onDeleteComment }) {
+    const [likeCount, setLikeCount] = useState(0);
+    
+    function handleDeleteComment() {
+        onDeleteComment(content);
+    };
+    
+    function handleLikeComment() {
+        setLikeCount((state) => {
+            return state + 1
+        });
+    }
+
+    return (
+        <div className={styles.comment}>
+            <Avatar hasBorder={false} src="https://avatars.githubusercontent.com/u/105370259?v=4"/>
+
+            <div className={styles.commentBox}>
+                <div className={styles.commentContent}>
+                    <header>
+                        <div className={styles.authorAndTime}>
+                            <strong>Giovany Inocêncio</strong>
+                            <time title="04 de Setembro às 19:39h" dateTime="2022-09-04 19:39:25">Cerca de 1h atrás</time>
+                        </div>
+                        <button onClick={handleDeleteComment} title="Deletar comentário">
+                            <Trash size={24} />
+                        </button>
+                    </header>
+                    <p>{content}</p>
+                </div>
+                <footer>
+                    <button onClick={handleLikeComment}>
+                        <ThumbsUp />
+                        Aplaudir <span>{likeCount}</span>
+                    </button>
+                </footer>
+            </div>
+        </div>
+    );
+}
